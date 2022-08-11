d5076854f7d2ffdb5597e65d6a9c86c3d3ebc65d
Ahmed <zaghrat.ahmed@gmail.com>
2022-08-11 18:34:33 +0200

    add Bootstrap

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,26 +17,48 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('firstname', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'attr' => ['class' => 'form-control']
+                ]
+            )
+            ->add(
+                'firstname',
+                TextType::class,
+                [
+                    'attr' => ['class' => 'form-control']
+                ]
+            )
+            ->add(
+                'lastname',
+                TextType::class,
+                [
+                    'attr' => ['class' => 'form-control']
+                ]
+            )
+            ->add(
+                'plainPassword',
+                PasswordType::class,
+                [
+                    // instead of being set onto the object directly,
+                    // this is read and encoded in the controller
+                    'mapped' => false,
+                    'attr' => ['autocomplete' => 'new-password', 'class' => 'form-control'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                ]
+            )
         ;
     }
 
